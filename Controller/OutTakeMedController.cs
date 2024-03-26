4d1807ced60d021797badf3dc8ef028393689c9d
vendors <vendors@TPSDD01>
2024-03-26 15:26:01 +0800

    20230326

--- Controller/OutTakeMedController.cs
+++ Controller/OutTakeMedController.cs
@@ -43,30 +43,88 @@ namespace DB2VM_API
         public string Post([FromBody] List<class_OutTakeMed_data> data)
         {
             if (data.Count == 0) return "";
-            string json = data.JsonSerializationt();
-            string str = "";
-            //SQLUI.SQLControl sQLControl = new SQLUI.SQLControl("127.0.0.1", "dbvm", "log", "user", "66437068", 3306, MySqlSslMode.None);
-            //object[] value = new object[3];
-            //value[0] = Guid.NewGuid().ToString();
-            //value[1] = DateTime.Now.ToDateTimeString();
-            //value[2] = data.JsonSerializationt(false);
-            //sQLControl.AddRow(null, value);
+            string json_out = "";
+            List<class_OutTakeMed_data> data_B1UD = (from temp in data
+                                                     where temp.成本中心.ToUpper() == "1"
+                                                     select temp).ToList();
+
+            List<class_OutTakeMed_data> data_B2UD = (from temp in data
+                                                     where temp.成本中心.ToUpper() == "2"
+                                                     select temp).ToList();
 
             if (data[0].成本中心 == "1")
             {
                 returnData returnData = new returnData();
                 returnData.ServerName = "B1UD";
-                returnData.Data = data;
-                str = Basic.Net.WEBApiPostJson("http://10.13.66.58:4433/api/OutTakeMed/new", returnData.JsonSerializationt());
+                returnData.Data = data_B1UD;
+                json_out = Basic.Net.WEBApiPostJson("http://10.13.66.58:4433/api/OutTakeMed/new", returnData.JsonSerializationt());
+                returnData = json_out.JsonDeserializet<returnData>();
+                if (returnData == null)
+                {
+                    return "NG";
+                }
+                if (returnData.Code != 200)
+                {
+                    return "NG";
+                }
             }
             if (data[0].成本中心 == "2")
             {
                 returnData returnData = new returnData();
                 returnData.ServerName = "B2UD";
-                returnData.Data = data;
-                str = Basic.Net.WEBApiPostJson("http://10.13.66.58:4433/api/OutTakeMed/new", returnData.JsonSerializationt());
+                returnData.Data = data_B2UD;
+                json_out = Basic.Net.WEBApiPostJson("http://10.13.66.58:4433/api/OutTakeMed/new", returnData.JsonSerializationt());
+                returnData = json_out.JsonDeserializet<returnData>();
+                if (returnData == null)
+                {
+                    return "NG";
+                }
+                if (returnData.Code != 200)
+                {
+                    return "NG";
+                }
             }
-            return str;
+            return "OK";
+        }
+
+        [Route("storehouse")]
+        [HttpPost]
+        public string Post_storehouse([FromBody] List<class_OutTakeMed_data> data)
+        {
+            List<class_OutTakeMed_data> data_B1UD = (from temp in data
+                                                     where temp.成本中心.ToUpper() == "B1UD"
+                                                     select temp).ToList();
+
+            List<class_OutTakeMed_data> data_B2UD = (from temp in data
+                                                     where temp.成本中心.ToUpper() == "B2UD"
+                                                     select temp).ToList();
+            if (data_B1UD.Count > 0)
+            {
+                returnData returnData = new returnData();
+                returnData.ServerName = "B1UD";
+                returnData.Data = data_B1UD;
+                string json_out = Basic.Net.WEBApiPostJson("http://10.13.66.58:4433/api/OutTakeMed/new", returnData.JsonSerializationt());
+                returnData = json_out.JsonDeserializet<returnData>();
+                if(returnData.Code != 200)
+                {
+                    return returnData.JsonSerializationt(true);
+                }
+           
+            }
+            if (data_B2UD.Count > 0)
+            {
+                returnData returnData = new returnData();
+                returnData.ServerName = "B2UD";
+                returnData.Data = data_B2UD;
+                string json_out = Basic.Net.WEBApiPostJson("http://10.13.66.58:4433/api/OutTakeMed/new", returnData.JsonSerializationt());
+                returnData = json_out.JsonDeserializet<returnData>();
+                if (returnData.Code != 200)
+                {
+                    return returnData.JsonSerializationt(true);
+                }
+
+            }
+            return "OK";
         }
     }
 }
